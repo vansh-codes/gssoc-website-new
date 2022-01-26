1435ff9360c20ae75fb851fdb610bae2e84c78cf
swapnilsparsh <arezona.lucky55@gmail.com>
2022-01-26 16:03:10 +0530

    Social Icons Done

--- components/homepage/SocialFollow.js
+++ components/homepage/SocialFollow.js
@@ -4,48 +4,52 @@ import {
   faYoutube,
   faFacebook,
   faTwitter,
-  faInstagramSquare,
-  faLinkedin
+  faInstagram,
+  faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
 
 export default function SocialFollow() {
   return (
-    <div className=" absolute object-right text-xs text-right right-0 pr-4 pl-4">
+    <div className=" absolute object-right text-xs text-right right-0 pr-4 pl-4 hidden sm:block">
       <h3 className="text-white">Social Follow</h3>
       <a
-        href="/"
+        href="https://www.facebook.com/girlscriptsoc/"
         target="_blank"
-        className="block pr-4 text-center text-black text-base bg-white hover:text-red-600 ease-linear hover:ease-in pt-4"
-      >
-        <FontAwesomeIcon className="w-10 h-10" icon={faYoutube} size="2x" />
-      </a>
-      <a
-        href="/"
-        target="_blank"
-        className="block pr-4 text-center text-black text-base bg-white hover:text-blue-600 ease-linear hover:ease-in pt-4"
+        className="block pr-4 text-center text-gray-700 text-base bg-white hover:text-blue-600 ease-linear hover:ease-in pt-4"
       >
         <FontAwesomeIcon className="w-10 h-10" icon={faFacebook} size="2x" />
       </a>
       <a
+        href="https://www.instagram.com/girlscriptsummerofcode/?hl=en"
+        target="_blank"
+        className="block pr-4 text-center text-gray-700 text-base bg-white hover:text-pink-600 ease-linear hover:ease-in pt-6"
+      >
+        <FontAwesomeIcon
+          className="w-10 h-10"
+          icon={faInstagram}
+          size="2x"
+        />
+      </a>
+      <a
         href="https://www.linkedin.com/company/girlscriptsoc/mycompany/"
         target="_blank"
-        className="block pr-4 text-center text-black text-base bg-white hover:text-blue-700 ease-linear hover:ease-in pt-4"
+        className="block pr-4 text-center text-gray-700 text-base bg-white hover:text-blue-700 ease-linear hover:ease-in pt-4"
       >
         <FontAwesomeIcon className="w-10 h-10" icon={faLinkedin} size="2x" />
       </a>
       <a
         href="https://twitter.com/girlscriptsoc"
         target="_blank"
-        className="block pr-4 text-center text-black text-base bg-white hover:text-blue-400 ease-linear hover:ease-in pt-4"
+        className="block pr-4 text-center text-gray-700 text-base bg-white hover:text-blue-400 ease-linear hover:ease-in pt-4"
       >
         <FontAwesomeIcon className="w-10 h-10" icon={faTwitter} size="2x" />
       </a>
       <a
-        href="https://www.instagram.com/girlscriptsummerofcode/?hl=en"
+        href="https://www.youtube.com/channel/UCBOlJtDcWNh0aUkS2CfI8Aw"
         target="_blank"
-        className="block pr-4 text-center text-black text-base bg-white hover:text-pink-600 ease-linear hover:ease-in pt-6"
+        className="block pr-4 text-center text-gray-700 text-base bg-white hover:text-red-600 ease-linear hover:ease-in pt-4"
       >
-        <FontAwesomeIcon className="w-10 h-10" icon={faInstagram} size="2x" />
+        <FontAwesomeIcon className="w-10 h-10" icon={faYoutube} size="2x" />
       </a>
     </div>
   );
