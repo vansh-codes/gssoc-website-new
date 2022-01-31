54886b0e9a8c7b3faf54803e8566dd4585a6e05e
swapnilsparsh <arezona.lucky55@gmail.com>
2022-01-31 23:10:43 +0530

    FAQ Page updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,7 +26,7 @@ export const Navbar = () => {
 
   return (
     <>
-      <nav className="flex items-center transition-colors flex-wrap bg-white drop-shadow-xl p-1 justify-center mb-24 dark:bg-slate-800 dark:text-primary_orange-0 dark:transition-colors sticky top-0 z-10">
+      <nav className="flex items-center transition-colors flex-wrap bg-white drop-shadow-xl p-1 justify-center mb-10 dark:bg-slate-800 dark:text-primary_orange-0 dark:transition-colors sticky top-0 z-10">
         <Link href="/">
           <a className="inline-flex items-center p-2 mr-4 ">
             <span className="text-xl text-orange font-bold uppercase tracking-wide  ml-8">
