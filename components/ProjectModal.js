084da52db1fc7de1a6d5a43b479da70fcb5d81a0
Anubhav <guptaanubhav907@gmail.com>
2022-02-02 22:44:18 +0530

    Style Modals and imporve resposivenessof it

--- components/ProjectModal.js
+++ components/ProjectModal.js
@@ -46,21 +46,21 @@ const ProjectModal = ({ currProject }) => {
       <Button onClick={onOpen}>Open Modal</Button>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+        <ModalContent className="dark:bg-slate-700 h-96">
+          <ModalHeader className="flex text-center justify-center text-orange-500 font-medium font-Rubik border-2	border-red-600 border-dashed">Modal Title</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
+          <ModalBody className="flex flex-col justify-evenly content-center text-orange-500"> 
           {currProject.project_id}
             {currProject.mentors_id.map((mentor, j) => {
               return(<p key={j}>{mentor.name}</p>);
             })}
           </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+          <ModalFooter className="flex text-center justify-centercontent-center bg-orange-500 dark:bg-orange-700 ">
+            {/* <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
-            </Button>
-            <Button variant="ghost">Secondary Action</Button>
+            </Button> */}
+            <Button className="mr-36" variant="ghost">View Projects</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
