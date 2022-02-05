f3b84b78641c1fb10bce73a0e1e081a981d8959a
swapnilsparsh <arezona.lucky55@gmail.com>
2022-02-05 12:25:43 +0530

    Schedule updated

--- components/homepage/Schedule.js
+++ components/homepage/Schedule.js
@@ -14,10 +14,10 @@ function Schedule() {
           <div className="flex flex-col md:grid grid-cols-12 text-gray-50">
             <div className="flex md:contents">
               <div className="col-start-2 col-end-4 mr-10 md:mx-auto relative">
-                <div className="h-full w-6 flex items-center justify-center">
+                <div className="h-full w-6 flex items-center justify-center"> 
                   <div className="h-full w-1 bg-orange-500 pointer-events-none"></div>
                 </div>
-                <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-orange-500 animate-bounce dark:bg-white shadow text-center">
+                <div className="w-6 h-6 absolute top-1/2 -mt-3 border-4 border-orange-500 rounded-full bg-white dark:bg-gray-700 shadow text-center">
                   <i className="fas fa-check-circle text-white"></i>
                 </div>
               </div>
@@ -74,7 +74,7 @@ function Schedule() {
                 <div className="h-full w-6 flex items-center justify-center">
                   <div className="h-full w-1 bg-orange-500 pointer-events-none"></div>
                 </div>
-                <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-orange-500 dark:bg-white shadow text-center">
+                <div className="w-6 h-6 absolute top-1/2 -mt-3 rounded-full bg-orange-500 animate-bounce dark:bg-white shadow text-center">
                   <i className="fas fa-exclamation-circle text-gray-400"></i>
                 </div>
               </div>
