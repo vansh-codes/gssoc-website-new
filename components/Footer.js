8fe75922079f8648cd8321752f994d83139eb232
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-02-06 22:55:16 +0530

    mehak's "changes"

--- components/Footer.js
+++ components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => {
         <div className="container px-4 mx-auto">
           <div className="md:flex md:flex-wrap md:-mx-4 py-6 md:pb-8">
             <div className="footer-info lg:w-1/3 md:px-4">
-              <h4 className="text-white text-2xl font-medium mb-4">
+             {/* <h4 className="text-white text-2xl font-medium mb-4">
                 Made with ❤️ by GirlScript India
-              </h4>
+  </h4>*/}
               <Link href="/">
                 <img
-                  className="cursor-pointer"
-                  src="https://user-images.githubusercontent.com/64256342/151364339-4cbebec2-07fc-4bfa-bbf6-639fea147aca.png"
+                  className="cursor-pointer mt-8"
+                  src="https://github.com/GSSoC-Web/gssoc-assets/blob/main/Logo/GS_logo_Black.svg?raw=true"
                 />
               </Link>
             </div>
@@ -21,7 +21,7 @@ const Footer = () => {
             <div className="md:w-2/3 lg:w-1/3 md:px-4 xl:pl-16 mt-12 lg:mt-0">
               <div className="sm:flex">
                 <div className="sm:flex-1">
-                  <h6 className="text-lg font-bold text-black underline uppercase mt-4 md:mt-0 mb-4">
+                  <h6 className="text-lg font-bold text-black uppercase mt-4 md:mt-0 mb-4 hover:underline">
                     ABOUT GSSOC
                   </h6>
                   <div>
@@ -46,8 +46,8 @@ const Footer = () => {
                   </div>
                 </div>
                 <div className="sm:flex-1 mt-4 sm:mt-0">
-                  <h6 className="text-lg font-bold text-black underline uppercase mt-4 md:mt-0 mb-4">
-                    Other
+                  <h6 className="text-lg font-bold text-black uppercase mt-4 md:mt-0 mb-4 hover:underline">
+                    LEGAL
                   </h6>
                   <div>
                     <a
@@ -74,7 +74,7 @@ const Footer = () => {
             </div>
 
             <div className="md:w-1/3 md:px-4 md:text-left mt-12 lg:mt-0">
-              <h5 className="text-lg text-white font-medium mb-4">
+              <h5 className="text-lg text-black font-medium mb-4">
                 GirlScript is the fastest growing tech-community in India. It is
                 a non-profit project brought to you by GirlScript Foundation to
                 help beginners in technology. Be a part, spread knowledge {"& "}
@@ -92,11 +92,11 @@ const Footer = () => {
         <div className="border-t border-solid border-gray-900 mt-4 py-4">
           <div className="container px-4 mx-auto">
             <div className="md:flex md:-mx-4 md:items-center">
-              <div className="md:flex-1 md:px-4 text-center md:text-left">
-                <p className="text-white">
+              <div className="md:flex-1 md:px-4 text-center ">
+                <p className="text-black">
                   &copy;{" "}
                   <strong>
-                    2022 GirlScript Foundation. All rights reserved.
+                  2022 GirlScript Foundation. Made with 🖤️ by GirlScript India. All rights reserved. 
                   </strong>
                 </p>
               </div>
