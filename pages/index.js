6e3dd19c1cb2e1a3012cffcbf19765ab6b2a8a1d
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-02-12 02:43:45 +0530

    added fixed width but responsive statscard countup

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,8 @@ import EllipseBox2 from "../components/BgAssets/EllipseBox2";
 import Hex1 from "../components/BgAssets/Hex1";
 import Hex2 from "../components/BgAssets/Hex2";
 import Map from "../components/BgAssets/Map";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
+import VisibilitySensor from "react-visibility-sensor";
 
 export default function Home() {
   const [mounted, setMounted] = useState(false);
@@ -205,7 +206,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-row justify-between items-center bg-[#FFECDE] dark:bg-black flex-wrap rounded-xl mb-24" id="about-gssoc">
+        <div
+          className="flex flex-row justify-between items-center bg-[#FFECDE] dark:bg-black flex-wrap rounded-xl mb-24"
+          id="about-gssoc"
+        >
           <div className="w-560px px-4 pt-3 lg:text-right rounded-xl md:w-1/2 lg:my-4 lg:px-10 lg:py-10 lg:w-1/2">
             {theme === "light" ? (
               <img
@@ -244,69 +248,123 @@ export default function Home() {
 
           <div className="pr-8 font-semibold px-4 pt-3 rounded-lg lg:my-4 lg:px-10 lg:py-10 ">
             <div className="dark:bg-darkmode_gray-0 dark:transition-colors bg-stone-50 shadow-lg rounded-lg my-1 px-1 w-560px">
-             <h1 className="mt-8 pt-6 pl-8 text-5xl text-primary_orange-0 3rem 3rem ">GSSoC 2021</h1>
-             <p className="pl-8 text-2xl text-primary_orange-0 1.5rem 2rem "> by the numbers</p>
-             <div className="grid grid-cols-2 gap-2 mb-4">
-                <div>
+              <h1 className="mt-8 pt-6 pl-8 text-5xl text-primary_orange-0 3rem 3rem ">
+                GSSoC 2021
+              </h1>
+              <p className="pl-8 text-2xl text-primary_orange-0 1.5rem 2rem ">
+                {" "}
+                by the numbers
+              </p>
+              <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-2 mb-4">
+                <div className="statscol">
                   <div className="mt-4 pt-4 pl-8">
-                <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-                  <CountUp 
-                  end={8}
-                  duration={1}
-                  />k+</h1>
-             <p className="text-2xl text-primary_orange-0 1.5rem 2rem "> Registrations</p>
-             </div>
-             <div className="mt-4 pt-4 pl-8">
-             <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-             <CountUp 
-                  end={135}
-                  duration={1}
-                  />+</h1>
-             <p className="text-2xl text-primary_orange-0 1.5rem 2rem "> Institutes</p>
-             </div>
-             <div className="mt-4 pt-4 pl-8">
-             <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-             <CountUp 
-                  end={10}
-                  duration={1}
-                  />k+</h1>
-             <p className=" mb-6 text-2xl text-primary_orange-0 1.5rem 2rem ">Total PRs</p>
-             </div>
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>
+                            {isVisible ? <CountUp end={8000} /> : null}+
+                          </div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className="text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      {" "}
+                      Registrations
+                    </p>
+                  </div>
+                  <div className="mt-4 pt-4 pl-8">
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>{isVisible ? <CountUp end={135} /> : null}+</div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className="text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      {" "}
+                      Institutes
+                    </p>
+                  </div>
+                  <div className="mt-4 pt-4 pl-8">
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>
+                            {isVisible ? <CountUp end={10000} /> : null}+
+                          </div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className=" mb-6 text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      Total PRs
+                    </p>
+                  </div>
                 </div>
 
-                <div>
+                <div className="statscol">
                   <div className="mt-4 pt-4 pl-8 pr-4">
-                <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-                <CountUp 
-                  end={5500}
-                  duration={1}
-                  />+</h1>
-             <p className="text-2xl text-primary_orange-0 1.5rem 2rem "> Participants</p>
-             </div>
-             <div className="mt-4 pt-4 pl-8">
-             <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-             <CountUp 
-                  end={14}
-                  duration={1}
-                  />
-             </h1>
-             <p className="text-2xl text-primary_orange-0 1.5rem 2rem "> Countries</p>
-             </div>
-             <div className="mt-4 pt-4 pl-8">
-             <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
-             <CountUp 
-                  end={100}
-                  duration={1}
-                  />+</h1>
-             <p className=" mb-6 text-2xl text-primary_orange-0 1.5rem 2rem ">Total Projects</p>
-             </div>
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>
+                            {isVisible ? <CountUp end={5500} /> : null}+
+                          </div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className="text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      {" "}
+                      Participants
+                    </p>
+                  </div>
+                  <div className="mt-4 pt-4 pl-8">
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>{isVisible ? <CountUp end={14} /> : null}+</div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className="text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      {" "}
+                      Countries
+                    </p>
+                  </div>
+                  <div className="mt-4 pt-4 pl-8">
+                    <h1 className="text-5xl text-primary_orange-0 3rem 3rem ">
+                      <VisibilitySensor
+                        partialVisibility
+                        offset={{ bottom: 0 }}
+                      >
+                        {({ isVisible }) => (
+                          <div>{isVisible ? <CountUp end={100} /> : null}+</div>
+                        )}
+                      </VisibilitySensor>
+                    </h1>
+                    <p className=" mb-6 text-2xl text-primary_orange-0 1.5rem 2rem ">
+                      Total Projects
+                    </p>
+                  </div>
                 </div>
-
-            
               </div>
-              </div>
+            </div>
 
-         {/*   {theme === "light" ? (
+            {/*   {theme === "light" ? (
               <img
                 className="mt-3 mb-3 md:mt-0"
                 id="Learn_more"
@@ -364,9 +422,9 @@ export default function Home() {
               alt="logo"
             />
             <div className="mt-12 md:mt-0">
-              <p className="font-sans font-medium text-4xl dark:text-white text-gray-800">
+              {/* <p className="font-sans font-medium text-4xl dark:text-white text-gray-800">
                 and more ...
-              </p>
+              </p> */}
             </div>
           </div>
         </div>
