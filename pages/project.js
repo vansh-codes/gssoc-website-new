ebbd22cf604d925dcc3d91e4d5433e8122c65044
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-02-18 23:11:56 +0530

    fix: ported search functionality of anubhav

--- pages/project.js
+++ pages/project.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { projectData } from "./api/projectsData";
 import Image from "next/image";
+import { useTheme } from "next-themes";
 import { Button, ButtonGroup } from "@chakra-ui/react";
-import ProjectModal from "../components/ProjectModal";
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { SimpleGrid } from "@chakra-ui/react";
+import { Spacer } from "@chakra-ui/react";
 import { Search2Icon } from "@chakra-ui/icons";
+
+// import {
+//   AlertDialog,
+//   AlertDialogBody,
+//   AlertDialogFooter,
+//   AlertDialogHeader,
+//   AlertDialogContent,
+//   AlertDialogOverlay,
+// } from '@chakra-ui/react'
+import ProjectModal from "../components/ProjectModal";
 import {
   Modal,
   ModalOverlay,
@@ -14,12 +28,11 @@ import {
   ModalCloseButton,
 } from "@chakra-ui/react";
 import { useDisclosure, Lorem } from "@chakra-ui/react";
-// import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
 
 const Project = () => {
-  const [data] = useState(projectData);
+  const [data, setData] = useState(projectData);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [searchtrm, setsearchtrm] = useState("");
   const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
 
@@ -34,18 +47,23 @@ const Project = () => {
     <>
       <section>
         {/* <div className="flex items-center justify-center">
-          <div>
-            <p className="font-serif text-center flex items-center text-2xl font-extrabold text-black-100">
-              <p className="text-primary_orange-0 text-5xl text center font-extrabold mb-10">
-                Projects - GSSOC&apos;21
-              </p>
+          <p className="font-serif text-center dark:text-white text-2xl font-extrabold text-black-100">
+            <p className="text-primary_orange-0 text-5xl text center font-extrabold mb-10">
+              Projects <span className="dark:text-white">for GSSoC 2022</span>
             </p>
-          </div>
-          <div>
-            <div>
-              <input type="search" name="search" id="search" />
-            </div>
-          </div>
+            "Unfinished projects are the symbol of progress, not of
+            imperfection."
+          </p>
+        </div>
+        <div className="flex flex-col justify-center content-center items-center mt-4 ">
+          <input
+            type="text"
+            placeholder="Search Projects"
+            className="h-20 w-5/12 pl-5 border-2	border-black dark:hover:bg-white"
+            onChange={(event) => {
+              setsearchtrm(event.target.value);
+            }}
+          ></input>
         </div> */}
         <div className="flex justify-between items-center px-24">
           <div>
@@ -70,30 +88,40 @@ const Project = () => {
               placeholder="search..."
               name="search"
               id="search"
+              onChange={(event) => {
+                setsearchtrm(event.target.value);
+              }}
             />
           </div>
         </div>
         <p className="dark:text-white font-sans text-4xl font-semibold text-black mt-20 px-24 mb-10">
           “Unfinished projects are a symbol of progress, not of imperfection.”
         </p>
-        {/* <br /> */}
-        <div className="flex flex-wrap w-100 justify-around gap-5">
-          {data.map((curElem, i) => {
-            return (
-              <div
-                className="flex items-center justify-between w-80"
-                key={i}
-                data-aos="flip-up"
-                data-aos-duration="800"
-              >
-                <div className="p-4 shadow">
-                  <div className="mb-2 overflow-hidden rounded-lg shadow-lg md:h-80 w-80 p-5 flex flex-col justify-between">
-                    <>
-                      {/*                 
+        <br />
+        <div className="flex flex-row justify-center flex-wrap items-center gap-x-40 gap-y-10 mt-9">
+          {data
+            .filter((curElem, i) => {
+              if (searchtrm == "") {
+                return curElem;
+              } else if (curElem.technology_used.includes(searchtrm)) {
+                return curElem;
+              }
+            })
+            .map((curElem, i) => {
+              return (
+                <div
+                  className="flex items-center justify-between w-80"
+                  key={i}
+                  data-aos="flip-up"
+                  data-aos-duration="800"
+                >
+                  <div className="shadow dark:bg-black rounded-lg">
+                    <div className="mb-2 overflow-hidden rounded-lg shadow-lg md:h-80 w-80 p-5 flex flex-col justify-between">
+                      <>
+                        {/*                 
                 <Button colorScheme='red' onClick={() => setIsOpen(true)}>
         Delete Customer
       </Button>
-
       <AlertDialog
         isOpen={isOpen}
         leastDestructiveRef={cancelRef}
@@ -104,11 +132,9 @@ const Project = () => {
             <AlertDialogHeader fontSize='lg' fontWeight='bold'>
               Delete Customer
             </AlertDialogHeader>
-
             <AlertDialogBody>
               Are you sure? You can't undo this action afterwards.
             </AlertDialogBody>
-
             <AlertDialogFooter>
               <Button ref={cancelRef} onClick={onClose}>
                 Cancel
@@ -121,9 +147,9 @@ const Project = () => {
         </AlertDialogOverlay>
       </AlertDialog> */}
 
-                      {/* <Button onClick={onOpen}>Open Modal</Button> */}
+                        {/* <Button onClick={onOpen}>Open Modal</Button> */}
 
-                      {/* <Modal isOpen={isOpen} onClose={onClose}>
+                        {/* <Modal isOpen={isOpen} onClose={onClose}>
                         <ModalOverlay />
                         <ModalContent>
                           <ModalHeader>Modal Title</ModalHeader>
@@ -131,7 +157,6 @@ const Project = () => {
                           <ModalBody>
                             <h6> Duh </h6>
                           </ModalBody>
-
                           <ModalFooter>
                             <Button colorScheme="blue" mr={3} onClick={onClose}>
                               Close
@@ -140,45 +165,54 @@ const Project = () => {
                           </ModalFooter>
                         </ModalContent>
                       </Modal> */}
-                      <ProjectModal currProject={curElem} />
 
-                      <div className="font-bold text-indigo-500 md:text-lg">
-                        {i + 1}. {curElem.project_name}
-                      </div>
-                      <p className="mb-3 text-sm text-gray-500 md:text-base md:mb-4">
-                        {curElem.owner_name}
-                      </p>
-                      <br />
-                      {curElem.mentors_id.map((mentor, j) => {
+                        <div className="font-bold text-primary_orange-0 md:text-lg">
+                          {i + 1}. {curElem.project_name}
+                        </div>
+                        <p className="mb-3 text-sm dark:text-white md:text-base md:mb-4">
+                          {curElem.owner_name}
+                        </p>
+
+                        {/* {curElem.mentors_id.map((mentor, j) => {
                         return (
                           <h6 className="mb-3 text-sm text-gray-400" key={j}>
                             {j + 1}. {mentor.name}
                           </h6>
                         );
-                      })}
-                      <br />
-                      <ButtonGroup
-                        className="flex flex-wrap gap-1"
-                        variant="outline"
-                        spacing="1"
-                      >
-                        {curElem.technology_used
-                          .split(",")
-                          .map((techStk, k) => {
-                            return (
-                              <Button size="sm" colorScheme="orange" key={k}>
-                                {techStk.trim()}
-                              </Button>
-                            );
-                          })}
-                      </ButtonGroup>
-                    </>
+                      })} */}
+
+                        <ButtonGroup
+                          className="flex flex-row flex-wrap w-fit h-fit gap-4 mb-9 pl-7"
+                          variant="outline"
+                          spacing="2"
+                        >
+                          <SimpleGrid columns={2} spacing={5} margin={1}>
+                            {curElem.technology_used
+                              .split(",")
+                              .map((techStk, k) => {
+                                return (
+                                  <Button
+                                    size="sm"
+                                    colorScheme="orange"
+                                    width="100%"
+                                    className="dark:hover:bg-slate-700 dark:bg-stone-800"
+                                    key={k}
+                                  >
+                                    {techStk.trim()}
+                                  </Button>
+                                );
+                              })}
+                          </SimpleGrid>
+                        </ButtonGroup>
+                      </>
+                    </div>
+                    <ProjectModal currProject={curElem} />
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
+        <Spacer mt={10} />
       </section>
     </>
   );
