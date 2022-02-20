5f02c151361837aa085cf3fbbe30c832eff73df4
swapnilsparsh <arezona.lucky55@gmail.com>
2022-02-20 17:34:11 +0530

    Canvas Update and class to function

--- pages/participant.js
+++ pages/participant.js
@@ -1,72 +1,84 @@
-import React, { Component } from "react";
-export class App extends Component {
-  state = {
-    profileImg:
-      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-  };
-  imageHandler = (e) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        this.setState({ profileImg: reader.result });
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  };
-  render() {
-    const { profileImg } = this.state;
-    return (
-      <div className="page">
-        {/* <div className="content">
-          <div>
-            <img
-              src=""
-              alt="bg"
-            />
-            <div className="img-holder absolute ">
-              <img src={profileImg} alt="" id="img" className="img" />
-            </div>
-          </div>
 
-          <input
-            type="file"
-            accept="image/*"
-            name="image-upload"
-            id="input"
-            onChange={this.imageHandler}
-          />
-          <div className="label"></div>
-        </div> */}
-        <div className="relative">
-          <div className="img-holder absolute top-0 w-[500px] h-[500px] bg-white">
-            <img
-              src={profileImg}
-              alt=""
-              id="img"
-              className="img w-[500px] h-[500px]"
-            />
-          </div>
-          <img
-            className="relative w-[845px]"
-            src="https://user-images.githubusercontent.com/64256342/154729113-203bdabb-4296-40bd-b677-8455bb58bed0.png"
-            alt="bg"
-          />
+import React, { useState, useEffect, useRef } from "react"
+
+const CatMeme = () => {
+	const [imgPreview, setImgPreview] = useState(null);
+  const [error, setError] = useState(false);
+  const [image, setImage] = useState(null)
+  const canvas = useRef(null)
+
+	const handleImageChange = (e) => {
+    setError(false);
+    const selected = e.target.files[0];
+    const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
+    if (selected && ALLOWED_TYPES.includes(selected.type)) {
+      let reader = new FileReader();
+      reader.onloadend = () => {
+        setImgPreview(reader.result);
+      };
+      reader.readAsDataURL(selected);
+    } else {
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    const catImage = new Image();
+    catImage.src = "https://thiscatdoesnotexist.com/"
+    catImage.onload = () => setImage(catImage)
+  }, [])
+
+  useEffect(() => {
+    if(image && canvas) {
+      const ctx = canvas.current.getContext("2d")
+      ctx.fillStyle = "black"
+      ctx.fillRect(0, 0, 400, 256 + 80)
+      ctx.drawImage(image, (400 - 256) / 2, 40)
+
+      ctx.font = "20px Comic Sans MS"
+      ctx.fillStyle = "white"
+      ctx.textAlign = "center"
+    }
+  }, [image, canvas])
+
+  return (
+    <div>
+      <div>
+        <canvas 
+          ref={canvas}
+          width={400}
+          height={256 + 80}
+        ></canvas>
+				 <div className="App">
+      <div className="container">
+        {error && <p className="errorMsg">File not supported</p>}
+        <div
+          className="imgPreview"
+          style={{
+            background: imgPreview
+              ? `url("${imgPreview}") no-repeat center/cover`
+              : "#131313"
+          }}
+        >
+          {!imgPreview && (
+            <>
+              <p>Add an image</p>
+              <label htmlFor="fileUpload" className="customFileUpload">
+                Choose file
+              </label>
+              <input type="file" id="fileUpload" onChange={handleImageChange} />
+              <span>(jpg, jpeg or png)</span>
+            </>
+          )}
         </div>
-        <input
-          type="file"
-          accept="image/*"
-          name="image-upload"
-          id="input"
-          onChange={this.imageHandler}
-        />
-        <div className="label"></div>
-        <label className="image-upload" htmlFor="input">
-          {/* <i className="material-icons">add_photo_alternate</i> */}
-          Choose your Photo
-        </label>
+        {imgPreview && (
+          <button onClick={() => setImgPreview(null)}>Remove image</button>
+        )}
       </div>
-    );
-  }
+    </div>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default CatMeme
