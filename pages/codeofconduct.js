886d8bd3b14dbeb8c50e31d3761b1486f204d3ac
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-03-04 01:53:39 +0530

    fix light mode

--- pages/codeofconduct.js
+++ pages/codeofconduct.js
@@ -14,16 +14,16 @@ const Content = () => {
         </div>
         {Datas.filter((data) => {
           return data;
-        }).map((data) => {
+        }).map((data, j) => {
           return (
-            <div class="container">
-              <div class="flex flex-col md:grid grid-cols-13 text-gray-50">
-                <div class="flex md:contents">
-                  <div class="list relative mb-10">
+            <div className="container" key={j}>
+              <div className="flex flex-col md:grid grid-cols-13 text-gray-50">
+                <div className="flex md:contents">
+                  <div className="list relative mb-10">
                     <span className="absolute z-[1] bg-code_of_conduct-1 px-7 py-2 rounded-full font-serif text-xl">
-                      {data.id}
+                      {j + 1}
                     </span>
-                    <h3 className="relative  mt-5 ml-5 bg-code_of_conduct-0 shadow-lg rounded-lg px-5 py-8 font-serif text-xl">
+                    <h3 className="relative mt-5 ml-5 bg-orange-100 text-black dark:bg-code_of_conduct-0 dark:text-white shadow-xl rounded-lg px-5 py-8 font-serif text-xl">
                       {data.head}
                     </h3>
                   </div>
