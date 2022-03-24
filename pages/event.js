9c35b5858107f1ca637dcc304774e6955b97ef19
Vishwa.R <v.i.s.h.w.a.08.11.2001@gmail.com>
2022-03-24 12:27:10 +0530

    feat: add events page ✔
    - new page at /event
    - new api json at api/event_data
    - responsive
    - themed

--- pages/event.js
+++ pages/event.js
@@ -1,23 +1,87 @@
+/* 
+IMPORTANT NOTE FOR DEVELOPERS WORKING ON THIS PAGE ⚠
+======================================================================================
+
+THIS PAGE USES REACT-CHRONO TO RENDER TIMELINES
+https://github.com/prabhuignoto/react-chrono
+
+ALL THE DATA RENDERED IN THIS FILE ARE FETCCHED FROM "./api/event_data" FILE.
+
+NOTE:
+======================================================================================
+THE DATE MUST HAVE THE KEY VALUE OF "title", TO REDNER IT AS A SEPERATE COMPONENT, AND ITS MANDATORY.
+
+THEMING THE CHRONO-COMPONENT WILL TAKE EFFECT IN BOTH THE DARK AND LIGHT MODES OF NEXT-THEME.
+
+TAILWIND DARK CLASSES WILL ONLY TAKE EFFECT INSIDE THE ACTUAL CHRONO-COMPONENT, SO IT'S NOT POSSIBLE TO CHANGE THE CHRONO-COMPONENT BASE COLORS WITH TAILWINDCSS.
+
+I'VE USED NEUTRAL COLOR THEMES FOR BASE CHRONO COMPONENT, SO THAT IT DOESN'T ODD OUT ON BOTH DARK AND LIGHT THEMES.
+*/
+
 import React from "react";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 import { Chrono } from "react-chrono";
-import data from "./api/event_data";
+import Eventdata from "./api/event_data";
 
 const event = () => {
+  const [Event] = useState(Eventdata);
+  // theme check
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+  const [mounted, setMounted] = useState(false);
+  const { theme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
   return (
     <div className="wrapper">
-      <div className="w-full h-[90vh]">
+      <h1 className="text-4xl font-sans text-center dark:text-white font-semibold mb-10">
+        Events in GSSoC'22
+      </h1>
+      <div className="w-full">
         <Chrono
-          items={data}
+          items={Eventdata}
           theme={{
-            primary: "orange",
-            secondary: "orange",
-            cardBgColor: "black",
+            primary: "#f67621",
+            secondary: "#f67621",
+            cardBgColor: "#f67621",
             cardForeColor: "white",
             titleColor: "white",
           }}
           hideControls="true"
+          // cardHeight={250}
           mode="VERTICAL_ALTERNATING"
-        />
+          cardHeight={350}
+          // scrollable={{ scrollbar: false }}
+        >
+          {Event.map((curEvent, i) => {
+            return (
+              <div className="wrapper font-sans">
+                <img src={curEvent.poster} alt="poster" />
+                <figcaption className="text-lg font-semibold">
+                  {curEvent.maintitle}
+                </figcaption>
+                <div>
+                  <p className="text-white">{curEvent.info}</p>
+                </div>
+                <div>
+                  <a
+                    className="text-black text-md font-semibold"
+                    href={curEvent.rec_link}
+                  >
+                    Link here »
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </Chrono>
       </div>
     </div>
   );
