46a0bf94c23f3cdd61114165bc17cc5a1257149c
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-06-25 19:22:06 +0530

    added certificate frontend :ocean:

--- pages/verify_cert.js
+++ pages/verify_cert.js
@@ -1,15 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { Box, Skeleton, SkeletonCircle, Spacer } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 import FileSaver from "file-saver";
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
+// import CertImg from "../components/cert.svg";
+import Image from 'next/image';
+
 const Cert = () => {
-  let [filter, setFilter] = useState("");
-  const downloadImage = () => {
-    var image_url = "https://res.cloudinary.com/dqjtoo2h2/image/upload/l_text:helvetica_72_bold_normal_left:"+filter+"/fl_layer_apply,g_center,x_0.2,y_-0.1/CA_Speaker_certificate_oqydmx.jpg"
-    FileSaver.saveAs(image_url, "Cert_GSSoC.png"); // Put your image url here.
-  };
+  const ref = useRef();
+  const [Name, setName] = useState("");
+  const downloadImage = 
+
+  // () => {
+  //   var image_url = "https://res.cloudinary.com/dqjtoo2h2/image/upload/co_rgb:FD7617,l_text:Playfair%20Display_80_bold_normal_left:"+Name+"/fl_layer_apply,g_center,x_0.2,y_-0.12/GSSoC2022_Cert_nf09fu.png"
+  //   FileSaver.saveAs(image_url, "Cert_GSSoC.png");
+  // }
+
+  /* Note to future devs 
+  Directly downloading doesn't seem to work because the CSS breaks when downloading th Blog.
+  Hence the source of download is entirely different
+  */
+  useCallback(() => {
+    if (ref.current === null) {
+      return
+    }
+
+    toPng(ref.current, { cacheBust: true, })
+      .then((dataUrl) => {
+        const link = document.createElement('a')
+        link.download = Name+"Cert GSSOC2022.png"
+        link.href = dataUrl
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [ref]);
   return (
     <>
       <Head>
@@ -23,40 +52,54 @@ const Cert = () => {
         />
       </Head>
       <div className="items-center justify-center">
-        <p className="font-sans text-center text-2xl font-extrabold text-black-100">
-          <p className="text-black text-5xl text center font-extrabold mb-10 dark:text-white">
+        <div className="font-sans text-center text-2xl font-extrabold text-black-100">
+          <div className="text-black text-5xl text center font-extrabold mb-10 dark:text-white">
             Verify Certificates
-          </p>
-        </p>
+          </div>
+        </div>
       </div>
       <Spacer mt={20} />
-      <div className="flex flex-col bg-white shadow-2xl dark:bg-black rounded-md px-0 sm:px-3 py-2 md:px-16 lg:py-4 mx-96 mb-10 relative inline-block w-2/3 md:w-auto">
-        <label className="text-black dark:text-primary_orange-0 font-semibold my-3 text-md">
-          Enter Certificate ID hash
+      <div className="flex flex-col bg-white shadow-2xl dark:bg-black rounded-md mx-2 sm:mx-10 md:mx-10 lg:mx-40 px-10 py-2">
+        <label className="text-black dark:text-primary_orange-0 font-semibold mt-3 text-lg">
+          Enter GitHub Username
         </label>
         <input
           type="text"
-          className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-md"
+          className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-lg"
         ></input>
-        <label className="text-black dark:text-primary_orange-0 font-semibold my-3 text-md">
+        <label className="text-black dark:text-primary_orange-0 font-semibold mt-3 text-lg">
           Enter Name
         </label>
         <input
           type="text"
-          onChange={(e) => {
-            setFilter(e.target.value);
-          }}
-          className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-md"
-          id="contrib_name"
+          className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-lg"
+          value={Name}
+          onChange={(e) => setName(e.target.value)}
         ></input>
-        <img
+        <label className="text-black dark:text-primary_orange-0 font-semibold mt-3 text-lg">
+          Select Role in GSSoC 2022
+        </label>
+        <select className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-lg">
+          <option value="">Contributor</option>
+          <option value="">Mentor</option>
+          <option value="">Project Admin</option>
+        </select>
+        <Spacer mt={20} />
+        {/* <img
           src="https://res.cloudinary.com/dqjtoo2h2/image/upload/l_text:helvetica_72_bold_normal_left:Contributor/fl_layer_apply,g_center,x_0.2,y_-0.1/CA_Speaker_certificate_oqydmx.jpg"
           className="w-full h-auto mt-4"
           id="canvas"
-        />
+        /> */}
+        <div className="flex justify-center" id="cert">
+          {/* <Image src="/cert.png" height="700" width="1000" alt="Certificate"/> */}
+          <div className="banner" ref={ref}>
+          <div id="contrib_name" className="contrib_name text-big-orange">{Name}</div>
+          </div>
+        </div>
+        <Spacer mt={20} />
         <button
           type="button"
-          className="bg-primary_orange-0 dark:bg-white font-semibold mt-2 text-xs sm:text-sm md:text-md"
+          className="bg-gradient-to-b from-primary_orange-0 to-orange-600 text-md text-white dark:text-black font-medium rounded-b-md hover:bg-gradient-to-t hover:from-primary_orange-0 hover:to-orange-600 text-md text-white font-bold px-5 py-1 rounded md:text-xl md:py-3"
           onClick={downloadImage}
         >
           Verify
