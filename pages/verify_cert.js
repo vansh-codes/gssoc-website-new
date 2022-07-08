5f06b9693c08c5cd3e3a35152411260b1e3935e7
Suvraneel Bhuin <bsuvraneel@gmail.com>
2022-07-08 21:32:18 +0530

    Sol Fx Switcher added :ant:

--- pages/verify_cert.js
+++ pages/verify_cert.js
@@ -1,32 +1,18 @@
-import { useState, useEffect, useCallback, useRef } from "react";
-import { Box, Skeleton, SkeletonCircle, Spacer } from "@chakra-ui/react";
+import { Spacer } from "@chakra-ui/react";
 import Head from "next/head";
-import Link from "next/link";
-import React from "react";
+import { useState } from "react";
 // import FileSaver from "file-saver";
 // import CertImg from "../components/cert.svg";
-import Image from "next/image";
 import dynamic from "next/dynamic";
 // const exportComponentAsPNG = dynamic(import('react-component-export-image'), { ssr: false });
-import html2canvas from "html2canvas";
-import { ethers } from "ethers";
-import keccak256 from "keccak256";
-import MerkleTree from "merkletreejs";
-import ABI from "./JSON/ABI.json";
-import Contributors from "./JSON/Contributors.json";
-import Top100 from "./JSON/Top100.json";
-import Mentors from "./JSON/Mentors.json";
-import CampusAmbassadors from "./JSON/CampusAmbassadors.json";
-import ProjectAdmins from "./JSON/ProjectAdmins.json";
-import OpenSourceAdvocates from "./JSON/OpenSourceAdvocates.json";
-import OrganizingTeam from "./JSON/OrganizingTeam.json";
+
 
 const Certi_Comp = dynamic(() => import("../components/Certi_Comp"), {
   ssr: false,
 });
-const contractAddress = "0x71a894ce35a8a4bfe05a0b967a77ae2da3b49a3f";
 
 const Cert = () => {
+  const [Email, setEmail] = useState("");
   const [Name, setName] = useState("");
   const [Role, setRole] = useState("Contributor");
 
@@ -56,63 +42,6 @@ const Cert = () => {
   //     })
   // }, [ref]);
 
-  const provider = new ethers.providers.JsonRpcProvider("JSON_RPC_PROVIDER");
-  const privateKey =
-    "0x2183467634e8e797c30f4a502ec8eab1a6e648ab8256668300092c4768bffc1d";
-  // add funds for ME please.xD
-
-  const wallet = new ethers.Wallet(privateKey, provider);
-  // console.log(wallet.address);
-  const contract = new ethers.Contract(contractAddress, ABI, provider);
-  // console.log(contract);
-  const contractWithWallet = contract.connect(wallet);
-
-  function treeMaker(file) {
-    let data = file;
-    let participantDataArray = [];
-    for (let key in data) {
-      participantDataArray.push(
-        keccak256(JSON.stringify(data[key])).toString("hex")
-      );
-    }
-    const leaves = participantDataArray.map((participant) =>
-      keccak256(participant)
-    );
-    const tree = new MerkleTree(leaves, keccak256, { sort: true });
-    return tree;
-  }
-
-  const projectAdminsTree = treeMaker(ProjectAdmins);
-  const mentorsTree = treeMaker(Mentors);
-  const contributorsTree = treeMaker(Contributors);
-  const top100Tree = treeMaker(Top100);
-  const campusAmbassadorsTree = treeMaker(CampusAmbassadors);
-  const openSourceAdvocatesTree = treeMaker(OpenSourceAdvocates);
-  const oranizingTeamTree = treeMaker(OrganizingTeam);
-
-  async function checker(tree, email) {
-    let singleParticipant = keccak256(
-      JSON.stringify({
-        email: email,
-      })
-    ).toString("hex");
-    const merkleProof = tree.getHexProof(keccak256(singleParticipant));
-    // console.log("merkleProof", merkleProof);
-    // console.log("singleParticipant", singleParticipant);
-    const tx = await contractWithWallet
-      .verifyOrgTeam(merkleProof, singleParticipant)
-      .then((res) => {
-        if (res == true) {
-          console.log("verified " + email);
-        }
-      });
-  }
-
-  function hello() {
-    console.log("hello");
-    checker(oranizingTeamTree, "harshghodkar@gmail.com");
-  }
-
   return (
     <>
       <Head>
@@ -134,13 +63,13 @@ const Cert = () => {
       </div>
       <Spacer mt={20} />
       <div className="flex flex-col bg-white shadow-2xl dark:bg-black rounded-md mx-2 sm:mx-10 md:mx-10 lg:mx-20 px-10 py-2">
-        <button onClick={hello}> Hello Blockchain</button>
         <label className="text-black dark:text-primary_orange-0 font-semibold mt-3 text-lg">
-          Enter GitHub Username
+          Enter Registered Email
         </label>
         <input
           type="text"
           className="text-primary_orange-0 dark:text-white font-semibold mt-2 text-xs sm:text-sm md:text-lg"
+          onChange={(e) => setEmail(e.target.value)}
         ></input>
         <label className="text-black dark:text-primary_orange-0 font-semibold mt-3 text-lg">
           Enter Name
@@ -165,6 +94,7 @@ const Cert = () => {
           <option value="Project Admin">Project Admin</option>
           <option value="Campus Ambassador">Campus Ambassador</option>
           <option value="Speaker">Speaker</option>
+          <option value="Organising Team">Organising Team</option>
         </select>
         <Spacer mt={20} />
         {/* <img
@@ -172,7 +102,7 @@ const Cert = () => {
           className="w-full h-auto mt-4"
           id="canvas"
         /> */}
-        <Certi_Comp Name={Name} Role={Role} />
+        <Certi_Comp Name={Name} Role={Role} Email={Email}/>
       </div>
     </>
   );
