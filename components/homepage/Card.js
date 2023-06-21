5e7b478f21675c6a6ff120b2c19893f1fd9251e4
Aman Manwani <aman04.manwani@gmail.com>
2023-06-21 20:16:30 +0530

    fixed cards allignment

--- components/homepage/Card.js
+++ components/homepage/Card.js
@@ -1,15 +1,17 @@
 function Cards(props) {
   return (
-    <div className="dark:bg-black bg-white shadow-lg rounded-lg  h-fit my-1 px-1 w-560px md:w-1/2 lg:my-4 lg:px-4 lg:w-2/5">
-      <article className="overflow-hidden rounded-lg">
-        <header className="flex items-center p-2 md:p-4">
-          <h1 className="dark:text-white font-semibold text-3xl 1.875 rem 2.25 rem">
-            {props.title}
-          </h1>
-        </header>
-        <p className="dark:text-white text-grey-darker font-normal text-xl 1.25rem 1.75rem p-2 md:p-4">
-          {props.content}
-        </p>
+    <div className="dark:bg-black flex bg-white shadow-lg rounded-lg  h-500px my-1 px-1 w-560px md:w-1/2 lg:my-4 lg:px-4 lg:w-2/5 ">
+      <article className="overflow-hidden rounded-lg flex justify-between flex-col">
+        <div>
+          <header className="flex items-center p-2 md:p-4">
+            <h1 className="dark:text-white font-semibold text-3xl 1.875 rem 2.25 rem">
+              {props.title}
+            </h1>
+          </header>
+          <p className="dark:text-white text-grey-darker font-normal text-xl 1.25rem 1.75rem p-2 md:p-4">
+            {props.content}
+          </p>
+        </div>
         <footer className="flex items-center justify-between leading-none p-2 md:p-4">
           <a
             className="w-full"
