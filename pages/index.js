fbd40a94f8890ac204b266bea97687c12f6654b5
Aman Manwani <aman04.manwani@gmail.com>
2023-06-27 23:38:52 +0530

    fixed ThemeBtn in md

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ export default function Home() {
         </div>
       </div>
       <SocialFollow />
-      <div className="absolute object-right text-xs text-right right-1 dark:bg-primary_orange-0 px-2 animate-pulse py-2 rounded-full opacity-80 bg-black top-20 md:hidden">
+      <div className="absolute object-right text-xs text-right right-1 dark:bg-primary_orange-0 px-2 animate-pulse py-2 rounded-full opacity-80 bg-black top-20 lg:hidden">
         <ThemeChanger />
       </div>
       <div className="container transition-colors mx-auto mt-12 mb-0 md:mb-12 p-8 sm:px-10 md:px-12 lg:px-40 2xl:px-50 dark:bg-darkmode_gray-0 dark:transition-colors ">
