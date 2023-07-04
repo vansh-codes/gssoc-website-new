952cd59f6360cf872e79169ae77e1e4d3e80ce02
Sh0bhit <sguptamds94@gmail.com>
2023-07-04 10:40:50 +0530

    fix(search-tags): limited tags to 2 rows

--- components/SearchTags.js
+++ components/SearchTags.js
@@ -5,12 +5,12 @@ export default function SearchTags({ setSearchTerm }) {
   const [selected, isSelected] = useState(-1);
 
   return (
-    <div className="flex flex-wrap  sm:gap-5 gap-2 justify-center md:w-[50%] sm:w-[70%] w-[90%] mx-auto dark:text-white text-black">
+    <div className="flex flex-wrap md:gap-5 gap-2 justify-center lg:w-[70%] w-[90%] mx-auto dark:text-white text-black">
       {projectTags.map((tags, idx) => {
         if (idx < 15) {
           return (
             <div
-              className={`border border-code_of_conduct-1 px-3 py-2 cursor-pointer sm:text-[15px] text-[10px] ${
+              className={`border border-code_of_conduct-1 sm:px-3 px-2 sm:py-2 py-1 cursor-pointer md:text-[15px] sm:text-[10px] text-[7px] ${
                 idx === selected ? "bg-code_of_conduct-1" : ""
               }`}
               key={tags.title}
