3641308000f6c815e1a3a9ea264f78c57db136d3
nawed2611 <nawed2611@gmail.com>
2023-07-09 23:37:48 +0530

    Add changes

--- pages/index.js
+++ pages/index.js
@@ -559,6 +559,7 @@ export default function Home() {
             >
               {/* Card-1 */}
               <Card
+                disabled
                 title="Start your Journey"
                 content="Participating in GSSoC’23 is an opportunity to upgrade your Github profile. Enrich yourselves to learn, explore, improve, enhance, build connections and become a team player to nourish your skills and personality. Don’t miss a chance to get well-versed with Open-Source and essential tools like Git and Github under the supervision of excellent mentors."
                 btntext="Apply as a Contributor"
@@ -567,6 +568,7 @@ export default function Home() {
               />
               {/* Card-2 */}
               <Card
+                disabled
                 title="Lead the Pack"
                 content="Mentors are the experts and take the lead of the open-source project team. They guide the fellow participants at each stage in a road map of the project. They are the backbone of team and are available throughout the summer to review the pull requests and suggest betterment. Apply to be a mentor at Girlscript summer of code and be the foundation of your team."
                 btntext="Apply as a mentor"
@@ -582,6 +584,7 @@ export default function Home() {
             <div className="flex flex-wrap justify-between md:justify-around -mx-1 lg:-mx-4">
               {/* Card-3 */}
               <Card
+                disabled
                 title="Inviting Projects &amp; NGOs"
                 content="If you are an organization with a project idea in mind, a website or an app then this is the golden opportunity to become a part of an awesome community. The budding developers under the guidance of expert mentors will work on your projects and strive to achieve the best version. Apply and let’s collaborate together because Together Everyone Achieves More."
                 btntext="Apply as an Organization/Project Admin"
