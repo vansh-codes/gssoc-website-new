f5e16a91e0dad6a2b12006ed15524ed26320245d
Kumar Priyanshu <109672617+kpriyanshu2003@users.noreply.github.com>
2024-04-28 19:14:10 +0530

    Updated : FAQs (#182)
    * Refactor Code
    
    * Added FAQ questions

--- pages/api/FAQdata.js
+++ pages/api/FAQdata.js
@@ -1,104 +1,144 @@
 export const accordionData = [
-    {
-        id: 1,
-        question: 'Will a beginner, with absolutely no knowledge of github, gain anything fruitful?',
-        answer: 'Yeah, definitely. The organization is meant to assist the beginners grow in the field of development. We’ll have distinct projects appropriate both for beginners as well as the accolades and thereby we’ll make sure that each and every participant gets to learn something new from the projects he or she is contributing for. '
-    },
-    {
-        id: 2,
-        question: 'Is there any registration fee?',
-        answer: 'No, there is no registration fees for participation. It is absolutely free of cost. '
-    },
-    {
-        id: 3,
-        question: 'Any age limit for participation?',
-        answer: 'No, there is no age limit for participation in GSSoC. It is open for all. '
-    },
-    {
-        id: 4,
-        question: 'How will we receive the prizes (if won) and certificate?',
-        answer: 'We’ll be providing all the participants with the digital certificate. The prize money to all the concerned students will be sent to their bank accounts. We’ll ask for the bank account details via an email after the conclusion of the event. '
-    },
-    {
-        id: 5,
-        question: 'By what time will the result come?',
-        answer: 'The results will be declared in the second week of August. '
-    },
-    {
-        id: 6,
-        question: 'What minimum conditions should the project satisfy?',
-        answer: 'There are no such conditions that the projects need to satisfy. The mentors can propose the idea in any field that they specifically expertise in. '
-    },
-    {
-        id: 7,
-        question: 'Is it strictly for girls or can boys also participate?',
-        answer: 'GirlScript aims to assist anyone who is new in the field of development and it specifically supports an inclusive environment. Thereby GSSoC is open both for girls and for boys. Everyone can participate and contribute their part in the development of open source community and our initiative. '
-    },
-    {
-        id: 8,
-        question: 'Where do we start as a beginner to contribute properly in the projects?',
-        answer: 'We’ll be providing you with the links on our official website to start with Github and various other development to make sure that everyone is able to aptly contribute for the projects. '
-    },
-    {
-        id: 9,
-        question: 'How will participants communicate with mentors and admins?',
-        answer: 'All communication will happen on the corresponding channel for the particular project in the official GSSoC ’22 Discord server. '
-    },
-    {
-        id: 10,
-        question: 'How will I know that I am selected?',
-        answer: 'The participants will be notified through email and the invite to the Discord server will be sent to them. '
-    },
-    {
-        id: 11,
-        question: 'What is the criterion for selection as a participant?',
-        answer: 'This program is for beginners to get started with open source. The only criterion for selection are your dedication and enthusiasm to contribute to open source. '
-    },
-    {
-        id: 12,
-        question: 'Is GSSoC exclusively for girls or can boys participate as well?',
-        answer: 'GirlScript aims to support anyone new to the development field and promotes inclusivity. GSSoC is open for all. Everyone is welcome to participate and contribute to the open-source community and our initiative.'
-    },
-    {
-        id: 13,
-        question: 'Want to learn GitHub? Start here!',
-        answer: `If you want to learn GitHub independently, you can start by referring to the official documentation available at: https://guides.github.com/introduction/flow/. Additionally, there are step-by-step tutorials available that provide a comprehensive understanding of how GitHub works.`
-    },
-    {
-        id: 14,
-        question: 'What is the role of a mentor and how do they assist?',
-        answer: `A mentor is an experienced individual who helps you navigate and solve any challenges you encounter during your contribution journey in GSSoC. 
-        They provide guidance, support, and expertise to ensure you have a fulfilling learning experience.`
-    },
-    {
-        id: 15,
-        question: 'Can I contribute to multiple projects?',
-        answer: `Yes, you can contribute to multiple projects. However, we recommend that you focus on one project at a time to ensure you have a fulfilling learning experience.`
-    },
-    {
-        id: 16,
-        question: 'Are projects assigned to us or can we freely choose?',
-        answer: `You can freely choose the project you want to contribute to here - https://gssoc.girlscript.tech/project.`
-    },
-    {
-        id: 17,
-        question: `Where can I find projects eligible for GSSoC'23?`,
-        answer: `You can find the list of projects eligible for GSSoC'23 here - https://gssoc.girlscript.tech/project.`
-    },
-    {
-        id: 18,
-        question: `What are Discord roles?`,
-        answer: `Discord roles are assigned to participants based on their involvement in GSSoC. If you are a contributor, you are required to add "| Contributor" to your name, and similar guidelines apply to other roles. These roles help facilitate communication and collaboration within the community. The specific rules regarding Discord roles are clearly outlined in the Discord channel. If you have multiple roles, such as being both a contributor and a mentor, prioritize the role that holds greater significance for you.`
-    },
-    {
-        id: 19,
-        question: `What should I do after completing GSSoC?`,
-        answer: `Congratulations on successfully finishing GSSoC! Give yourself a pat on the back, as it is an incredible achievement. Be proud of yourself! 
-        Now, as you continue your open-source journey, you have various options. You can contribute to larger projects on GitHub, collaborate with other developers, or even create your own projects to address specific problems. The possibilities are endless, and this is just the beginning of your exciting journey in the world of open source.`
-    },
-    {
-        id: 20,
-        question: `Is your question not listed here?`,
-        answer: `If you cannot find the answer to your question in the FAQ section, please reach out to us via email, and we will respond as soon as possible. You can email us at gssoc@girlscript.tech. Additionally, you can also ask your question in the official GSSoC Discord channel, in the respective channel dedicated to discussions and queries.`
-    }
+  {
+    id: 1,
+    question:
+      "Will a beginner, with absolutely no knowledge of github, gain anything fruitful?",
+    answer:
+      "Yeah, definitely. The organization is meant to assist the beginners grow in the field of development. We'll have distinct projects appropriate both for beginners as well as the accolades and thereby we'll make sure that each and every participant gets to learn something new from the projects he or she is contributing for. ",
+  },
+  {
+    id: 2,
+    question: "Is there any registration fee?",
+    answer:
+      "No, there is no registration fees for participation. It is absolutely free of cost. ",
+  },
+  {
+    id: 3,
+    question: "Any age limit for participation?",
+    answer:
+      "No, there is no age limit for participation in GSSoC. It is open for all. ",
+  },
+  {
+    id: 4,
+    question: "How will we receive the prizes (if won) and certificate?",
+    answer:
+      "We'll be providing all the participants with the digital certificate. The prize money to all the concerned students will be sent to their bank accounts. We'll ask for the bank account details via an email after the conclusion of the event. ",
+  },
+  {
+    id: 5,
+    question: "By what time will the result come?",
+    answer: "The results will be declared in the second week of August. ",
+  },
+  {
+    id: 6,
+    question: "What minimum conditions should the project satisfy?",
+    answer:
+      "There are no such conditions that the projects need to satisfy. The mentors can propose the idea in any field that they specifically expertise in. ",
+  },
+  {
+    id: 7,
+    question: "Is it strictly for girls or can boys also participate?",
+    answer:
+      "GirlScript aims to assist anyone who is new in the field of development and it specifically supports an inclusive environment. Thereby GSSoC is open both for girls and for boys. Everyone can participate and contribute their part in the development of open source community and our initiative. ",
+  },
+  {
+    id: 8,
+    question:
+      "Where do we start as a beginner to contribute properly in the projects?",
+    answer:
+      "We'll be providing you with the links on our official website to start with Github and various other development to make sure that everyone is able to aptly contribute for the projects. ",
+  },
+  {
+    id: 9,
+    question: "How will participants communicate with mentors and admins?",
+    answer:
+      "All communication will happen on the corresponding channel for the particular project in the official GSSoC '22 Discord server. ",
+  },
+  {
+    id: 10,
+    question: "How will I know that I am selected?",
+    answer:
+      "The participants will be notified through email and the invite to the Discord server will be sent to them. ",
+  },
+  {
+    id: 11,
+    question: "What is the criterion for selection as a participant?",
+    answer:
+      "This program is for beginners to get started with open source. The only criterion for selection are your dedication and enthusiasm to contribute to open source. ",
+  },
+  {
+    id: 12,
+    question: "Is GSSoC exclusively for girls or can boys participate as well?",
+    answer:
+      "GirlScript aims to support anyone new to the development field and promotes inclusivity. GSSoC is open for all. Everyone is welcome to participate and contribute to the open-source community and our initiative.",
+  },
+  {
+    id: 13,
+    question: "Want to learn GitHub? Start here!",
+    answer: `If you want to learn GitHub independently, you can start by referring to the official documentation available at: https://guides.github.com/introduction/flow/. Additionally, there are step-by-step tutorials available that provide a comprehensive understanding of how GitHub works.`,
+  },
+  {
+    id: 14,
+    question: "What is the role of a mentor and how do they assist?",
+    answer: `A mentor is an experienced individual who helps you navigate and solve any challenges you encounter during your contribution journey in GSSoC. 
+        They provide guidance, support, and expertise to ensure you have a fulfilling learning experience.`,
+  },
+  {
+    id: 15,
+    question: "Can I contribute to multiple projects?",
+    answer: `Yes, you can contribute to multiple projects. However, we recommend that you focus on one project at a time to ensure you have a fulfilling learning experience.`,
+  },
+  {
+    id: 16,
+    question: "Are projects assigned to us or can we freely choose?",
+    answer: `You can freely choose the project you want to contribute to here - https://gssoc.girlscript.tech/project.`,
+  },
+  {
+    id: 17,
+    question: `Where can I find projects eligible for GSSoC'23?`,
+    answer: `You can find the list of projects eligible for GSSoC'23 here - https://gssoc.girlscript.tech/project.`,
+  },
+  {
+    id: 18,
+    question: `What are Discord roles?`,
+    answer: `Discord roles are assigned to participants based on their involvement in GSSoC. If you are a contributor, you are required to add "| Contributor" to your name, and similar guidelines apply to other roles. These roles help facilitate communication and collaboration within the community. The specific rules regarding Discord roles are clearly outlined in the Discord channel. If you have multiple roles, such as being both a contributor and a mentor, prioritize the role that holds greater significance for you.`,
+  },
+  {
+    id: 19,
+    question: `What should I do after completing GSSoC?`,
+    answer: `Congratulations on successfully finishing GSSoC! Give yourself a pat on the back, as it is an incredible achievement. Be proud of yourself! 
+        Now, as you continue your open-source journey, you have various options. You can contribute to larger projects on GitHub, collaborate with other developers, or even create your own projects to address specific problems. The possibilities are endless, and this is just the beginning of your exciting journey in the world of open source.`,
+  },
+  {
+    id: 20,
+    question: `Is your question not listed here?`,
+    answer: `If you cannot find the answer to your question in the FAQ section, please reach out to us via email, and we will respond as soon as possible. You can email us at gssoc@girlscript.tech. Additionally, you can also ask your question in the official GSSoC Discord channel, in the respective channel dedicated to discussions and queries.`,
+  },
+  {
+    id: 21,
+    question: `Can I be a contributor to projects too if I am a core team member?`,
+    answer: `Yes, you can be a contributor as well.`,
+  },
+  {
+    id: 22,
+    question: `Can we switch our roles after getting onboarded?`,
+    answer: `Sadly, No. You can't switch. If there is a requirement and your skillset matches, we may ask you to contribute to different teams.`,
+  },
+  {
+    id: 23,
+    question: `How can the core members contribute to GSSoC's program?`,
+    answer: `The roadmap has been shared above, can look into it. PMs will be in touch with you regarding the tasks.`,
+  },
+  {
+    id: 24,
+    question: `If we want to add something in GitHub org as our suggestion, do we need to raise a PR?`,
+    answer: `Yes, ideally that's the right way if you want to learn how things work refer https://recodehive.com/github-tutorials/`,
+  },
+  {
+    id: 25,
+    question:
+      "Registration form asks for a referral code. What should I fill in if I don't have one?",
+    answer:
+      "Referral codes are optional, it's the campus ambassador who passes on the referral codes. You can go ahead; this doesn't affect your application.",
+  },
 ];
--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import React, { useState } from "react";
-import DiabloAccordion from "../components/diabloAccordion";
 import { accordionData } from "./api/FAQdata";
+import DiabloAccordion from "../components/diabloAccordion";
 
 function Faq() {
   const [data] = useState(accordionData);
@@ -13,31 +13,28 @@ function Faq() {
         </title>
         <meta name="description" content="FAQs of GirlScript Summer of Code" />
       </Head>
-      <section className="flex flex-col items-center">
-        <p className="text-primary_orange-0 dark:text-white font-sans text-3xl md:text-5xl text center font-extrabold flex wrap justify-center flex-col md:flex-row mb-10 underline decoration-orange-500  underline-offset-8">
+      <>
+        <div className="dark:text-white font-sans text-3xl md:text-5xl text center font-extrabold flex wrap justify-center flex-col md:flex-row underline decoration-orange-500  underline-offset-8">
           <h1 className="text-primary_orange-0">
             Frequently Asked Questions&nbsp;
           </h1>
           <h1>(FAQs) </h1>
-        </p>
-        <br />
-        <div className="flex justify-around md:gap-1 w-11/12">
-          <div className="w-6/12">
-            {data.slice(0, Math.ceil(data.length / 2)).map((curElem) => {
-              const { id } = curElem;
-              return <DiabloAccordion key={id} {...curElem} />;
-            })}
+        </div>
+        <div className="flex md:gap-1 w-full justify-center px-16 mt-20 mb-10">
+          <div className="w-1/2">
+            {data.slice(0, Math.ceil(data.length / 2)).map((curElem) => (
+              <DiabloAccordion key={curElem.id} {...curElem} />
+            ))}
           </div>
-          <div className="w-6/12">
+          <div className="w-1/2">
             {data
               .slice(Math.ceil(data.length / 2), data.length)
-              .map((curElem) => {
-                const { id } = curElem;
-                return <DiabloAccordion key={id} {...curElem} />;
-              })}
+              .map((curElem) => (
+                <DiabloAccordion key={curElem.id} {...curElem} />
+              ))}
           </div>
         </div>
-      </section>
+      </>
     </>
   );
 }
