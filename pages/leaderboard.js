ccd6dc2b2abb00abbc00c0e64e51eb5f92a7470b
partha120804 <138643473+partha120804@users.noreply.github.com>
2024-06-24 21:45:06 +0530

    fixed (#277)
    changed the search input to a normal text input and added an extra clear input button

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -492,21 +492,15 @@ function Leaderboard() {
                       <input
                         onChange={(e) => {
                           setFilter(e.target.value);
-                          // if (e.target.value === ""){
-                          //   filterData()
-                          // }
-                          // else{
-                          //   filterData()
-                          // }
-                            filterData()
+                          filterData();
                         }}
-                        type="search"
+                        value={filter}
+                        id="searchInputField"
+                        type="text"
                         className="form-control relative flex-auto min-w-0 block px-0.5 py-1.5 text-base dark:bg-neutral-800 dark:text-neutral-100 dark:border-neutral-600 font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300  transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-gray-400 focus:outline-none dark:placeholder-neutral-300"
                         placeholder="Search"
-                        aria-label="Search"
-                        aria-describedby="button-addon2"
-                        onKeyUp={(e) => {
-                          e.key === "Enter" ? filterData() : filterData();
+                        onKeyUp={() => {
+                          filterData();
                         }}
                       />
                       <span className="search-count dark:text-neutral-300">
@@ -514,15 +508,46 @@ function Leaderboard() {
                       </span>
                     </div>
                     <button
+                      onMouseDown={() => {
+                        setFilter("");
+                      }}
+                      onMouseUp={() => {
+                        filterData();
+                      }}
+                      className="btn inline-block px-6 py-2.5 bg-gray-300 dark:bg-neutral-600 text-gray-600 font-medium text-xs leading-tight uppercase hover:text-gray-800 focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex items-center"
+                      type="button"
+                      id="clearSearch"
+                      style={{
+                        padding: "10px 10px",
+                        maxWidth: "35px",
+                        width: "20%",
+                      }}
+                    >
+                      <svg
+                    className="w-4 fill-neutral-600 hover:fill-neutral-800 dark:fill-neutral-300 dark:hover:fill-neutral-100"
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="fas"
+                    data-icon="search"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                  >
+                    <path d="M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4  L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1  c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1  c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z"></path>
+                  </svg>
+                    </button>
+                    <button
                       onClick={() => {
                         filterData();
                       }}
                       className="btn inline-block px-6 py-2.5 bg-gray-300 dark:bg-neutral-600 text-gray-600 font-medium text-xs leading-tight uppercase hover:text-gray-800 focus:outline-none focus:ring-0 transition duration-150 ease-in-out flex items-center"
                       type="button"
-                      id="button-addon2"
+                      id="searchButton"
+                      aria-label="Search"
+                      aria-describedby="button-addon2"
                       style={{
-                        padding: "10px 18px",
-                        maxWidth: "50px",
+                        padding: "10px 10px",
+                        maxWidth: "45px",
                         width: "20%",
                       }}
                     >
