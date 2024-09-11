3263d851d89d8430229fad824090430b8e1b5037
Hemu21 <rajva5727@gmail.com>
2024-09-11 14:35:15 +0530

    Sponser page test final
    Co-authored-by: Hemu21 <rajva5727@gmail.com>
    Co-authored-by: sanjay-kv <sanjay.kanakkot@gmail.com>
    Co-authored-by: MastanSayyad <sayyadmastan78@gmail.com>
    Co-authored-by: suhanipaliwal <suhanispaliwal@gmail.com>
    Co-authored-by: RadhikaMalpani1702 <radhikavishnudas2002@gmail.com>

--- pages/api/sponsor.js
+++ pages/api/sponsor.js
@@ -177,13 +177,20 @@ export default async function handler(req, res) {
       } catch (emailError) {
         console.error("Error sending email:", emailError);
       }
-
-      res.status(200).json({
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+      );
+      return res.status(200).json({
         message: "Sponsor data saved successfully. Email sending attempted.",
       });
     } catch (err) {
       console.error("Error saving sponsor data:", err);
-      res.status(500).json({ error: "Error saving sponsor data",message: err });
+      return res
+        .status(500)
+        .json({ error: "Error saving sponsor data", message: err });
     }
   } else {
     console.warn(`Method ${req.method} not allowed`);
