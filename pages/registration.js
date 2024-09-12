0905bc1af08684ab651fd3ca08aef8d203d7d8b4
Shreyash <shreyash3087@gmail.com>
2024-09-12 12:14:01 +0530

    Addded Seconds to the Countdown with Text to Indicate Hours,Mins,Second

--- pages/registration.js
+++ pages/registration.js
@@ -27,16 +27,27 @@ const Registration = () => {
   const [selectedTimer, setSelectedTimer] = useState({
     hours: "00",
     minutes: "00",
+    seconds: "00",
   });
-  const [caTimeLeft, setCaTimeLeft] = useState({ hours: "00", minutes: "00" });
-  const [paTimeLeft, setPaTimeLeft] = useState({ hours: "00", minutes: "00" });
+  const [caTimeLeft, setCaTimeLeft] = useState({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
+  const [paTimeLeft, setPaTimeLeft] = useState({
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  });
   const [contributorTimeLeft, setContributorTimeLeft] = useState({
     hours: "00",
     minutes: "00",
+    seconds: "00",
   });
   const [mentorTimeLeft, setMentorTimeLeft] = useState({
     hours: "00",
     minutes: "00",
+    seconds: "00",
   });
   const [isTimeUp, setIsTimeUp] = useState(false);
   useEffect(() => {
@@ -61,8 +72,10 @@ const Registration = () => {
         const minutes = String(
           Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
         ).padStart(2, "0");
-
-        return { hours: totalHours, minutes };
+        const seconds = String(
+          Math.floor((distance % (1000 * 60)) / 1000)
+        ).padStart(2, "0");
+        return { hours: totalHours, minutes, seconds };
       };
       setCaTimeLeft(calculateTimeLeft(caTargetDate));
       setPaTimeLeft(calculateTimeLeft(paTargetDate));
@@ -249,6 +262,7 @@ const Registration = () => {
   const timerDigits = [
     ...selectedTimer.hours.split(""),
     ...selectedTimer.minutes.split(""),
+    ...selectedTimer.seconds.split(""),
   ];
   const renderForm = () => {
     switch (currentStep) {
@@ -297,29 +311,45 @@ const Registration = () => {
             </div>
             {!isTimeUp && (
               <div>
-                <h1 className="text-2xl font-semibold text-center mb-12">
-                Are You Excited? Countdown with Us
+                <h1 className="text-2xl font-normal text-center max-[400px] mt-20 mb-16 w-full max-w-3xl m-auto">
+                  <span className="font-bold text-[#f57d33]">
+                    Are you excited?
+                  </span>{" "}
+                  The countdown to become a <span className="font-bold text-[#f57d33]">Campus Ambassador</span> with us has
+                  started!
                 </h1>
                 <div className="flex gap-6 max-sm:gap-3 max-sm:mt-20 items-center">
                   {timerDigits.map((digit, index) => (
                     <div
-                      className="flex justify-center items-center gap-4"
+                      className="flex flex-col justify-center items-center gap-4"
                       key={index}
                     >
-                      <div className="relative">
-                        <div className="bg-[#f57d33] w-24 h-24 max-sm:w-16 max-sm:h-16 border-[#f57d33] z-20 border-2 rounded-xl flex items-center justify-center"></div>
-                        <div className="bg-white absolute bottom-2 right-2 z-10 w-24 h-24 max-sm:w-16 max-sm:h-16 border-[#f57d33] border-2 rounded-xl">
-                          <span className="text-black text-2xl font-bold flex justify-center items-center h-full">
-                            {digit}
-                          </span>
+                      <div className="flex justify-center items-center gap-4">
+                        <div className="relative">
+                          <div className="bg-[#f57d33] w-24 h-24 max-sm:w-12 max-sm:h-12 max-[400px]:h-10 max-[400px]:w-10 border-[#f57d33] z-20 border-2 rounded-xl flex items-center justify-center"></div>
+                          <div className="bg-white absolute bottom-2 right-2 z-10 w-24 h-24 max-sm:w-12 max-sm:h-12 max-[400px]:h-10 max-[400px]:w-10 border-[#f57d33] border-2 rounded-xl">
+                            <span className="text-black text-2xl max-sm:font-semibold font-bold flex justify-center items-center h-full">
+                              {digit}
+                            </span>
+                          </div>
                         </div>
+                        {index % 2 !== 0 &&
+                          index !== timerDigits.length - 1 && (
+                            <div className="flex flex-col gap-4">
+                              <div className="bg-[#f57d33] w-4 h-4 max-sm:w-2 max-sm:h-2 rounded-full"></div>
+                              <div className="bg-[#f57d33] w-4 h-4 max-sm:w-2 max-sm:h-2 rounded-full"></div>
+                            </div>
+                          )}
                       </div>
-                          {index === timerDigits.length - 3 && (
-                        <div className="flex flex-col gap-4">
-                          <div className="bg-[#f57d33] w-4 h-4 max-sm:w-3 max-sm:h-3 rounded-full"></div>
-                          <div className="bg-[#f57d33] w-4 h-4 max-sm:w-3 max-sm:h-3 rounded-full"></div>
-                        </div>
-                      )}
+                      <div className="text-center font-normal text-[#676767] text-2xl">
+                        {index === 0 || index === 1 ? (
+                          <span>H</span>
+                        ) : index === 2 || index === 3 ? (
+                          <span>M</span>
+                        ) : (
+                          <span>S</span>
+                        )}
+                      </div>
                     </div>
                   ))}
                 </div>
@@ -659,7 +689,10 @@ const Registration = () => {
 
               <div className="flex flex-col md:flex-row items-center">
                 <div className="w-72 h-72 md:w-96 md:h-[450px] z-30 my-12">
-                  <img src="https://github.com/user-attachments/assets/451e5965-5142-4b13-bcfe-95ce77f7cd36" alt="Banner" />
+                  <img
+                    src="https://github.com/user-attachments/assets/451e5965-5142-4b13-bcfe-95ce77f7cd36"
+                    alt="Banner"
+                  />
                 </div>
 
                 <div className="p-4 md:p-8 max-w-lg md:max-w-3xl w-full z-10">
