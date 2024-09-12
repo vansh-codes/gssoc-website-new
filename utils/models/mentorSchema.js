4ef59bade173fc6a33c48110036cf1614346c1b1
Shreyash <shreyash3087@gmail.com>
2024-09-13 03:46:39 +0530

    Add nodemailer and mails for every role| Add and fix contributors,admins and mentors registrations form| Add new fields for resume upload
    Co-authored-by: shreyash3087 <shreyash3087@gmail.com>
    Co-authored-by: sanjay-kv <sanjay.kanakkot@gmail.com>

--- utils/models/mentorSchema.js
+++ utils/models/mentorSchema.js
@@ -28,7 +28,7 @@ const mentorSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-  referralCode: {
+  referral: {
     type: String,
     trim: true
   },
@@ -39,6 +39,7 @@ const mentorSchema = new mongoose.Schema({
   },
   linkedInProfileUrl: {
     type: String,
+    required: true,
     trim: true
   },
   discordUsername: {
@@ -48,12 +49,10 @@ const mentorSchema = new mongoose.Schema({
   },
   country: {
     type: String,
-    required: true,
     trim: true
   },
   state: {
     type: String,
-    required: true,
     trim: true
   },
   city: {
@@ -77,7 +76,17 @@ const mentorSchema = new mongoose.Schema({
     type: String,
     required: true,
     trim: true
-  }
+  },
+  resumeUpload: {
+    type: String,
+    trim: true
+  },
+  startupServices: {
+    type: String,
+    enum: ['', 'YES', 'NO'],
+    default: ''
+  },
+  
 }, { timestamps: true });
 
 const Mentor = mongoose.models.mentor || mongoose.model("mentor", mentorSchema);
