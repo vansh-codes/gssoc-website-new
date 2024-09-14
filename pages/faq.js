7713274b1db5585a0ccd1e9f3c7dc227926c3024
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-09-15 03:40:40 +0530

    Fixes\ dark mode refresh issue in FAQ page
    Co-authored-by: vansh-codes <vanshchaurasiya1557@gmail.com>
    Co-authored-by: MastanSayyad <sayyadmastan78@gmail.com>

--- pages/faq.js
+++ pages/faq.js
@@ -15,7 +15,7 @@ function Faq() {
         <meta name="description" content="FAQs of GirlScript Summer of Code" />
       </Head>
       <>
-      <div className="flex flex-row dark:text-white font-sans text-3xl md:text-5xl text-center font-extrabold flex-wrap justify-center items-center">
+      <div className="flex flex-row mt-4 dark:text-white font-sans text-3xl md:text-5xl text-center font-extrabold flex-wrap justify-center items-center">
        <h1 className=" underline">
        <span style={{ color:'#f97316' }}>Frequently Asked Questions</span>
         </h1>
@@ -23,7 +23,7 @@ function Faq() {
       </div>
 
 
-        <div className="flex flex-col w-full justify-center px-16 mt-20 mb-10">
+        <div className="flex flex-col w-full justify-center px-16 mt-16 mb-10">
           {data.map((curElem) => (
             <DiabloAccordion key={curElem.id} {...curElem} />
           ))}
